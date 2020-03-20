6b39b12f2cdac2fc8723e8c797ac0550623490dd
Ivan Procaccini <ivanprocaccini905@gmail.com>
2020-03-20 10:26:13 +0000

    Commit iniziale
    Aggiunto sostanzialmente il codice standard di Jekyll, con piccole
    modifiche iniziali (tra cui il tema)

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Post di prova di Più Europa 4.0",
+        "excerpt":"Lascio qui le zozzatine standard di Jekyll per il momento. You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve,...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "http://localhost:4000/jekyll/update/2020/03/20/welcome-to-jekyll.html",
+        "teaser": null
+      }]
